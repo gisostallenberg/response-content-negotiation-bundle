b65c2bc1961f061911a11d3425d48f64c26a7d7e
Jaap Romijn <jaap@connectholland.nl>
2020-02-20 15:21:30 +0100

    Result service locator interface

---
+++ src/Content/ResultServiceLocatorInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Response Content Negotiation Bundle package.
+ * (c) Giso Stallenberg.
+ */
+
+namespace GisoStallenberg\Bundle\ResponseContentNegotiationBundle\Content;
+
+use Symfony\Component\DependencyInjection\ServiceSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+interface ResultServiceLocatorInterface extends ServiceSubscriberInterface
+{
+    public function getResult(Request $request, ResultDataInterface $resultData): ResultInterface;
+}
